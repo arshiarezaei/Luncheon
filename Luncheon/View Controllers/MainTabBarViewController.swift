5879dfc6e9262977163072798f5c47577590fbb9
Arshiya Rezaie <arshiyarezaie@gmail.com>
2020-02-25 20:22:53 +0330

    -MainTabBar added -first version of MainTabBarViewController class implemented .

---
+++ Luncheon/View Controllers/MainTabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarViewController.swift
+//  Luncheon
+//
+//  Created by Arshiya on 12/6/1398 AP.
+//  Copyright © 1398 AP ir.luncheon. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.tintColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
