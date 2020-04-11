99d7e38cb63b62c80e86a504b1c5bb0c01ff0b18
Arshiya Rezaie <arshiyarezaie@gmail.com>
2020-04-11 15:56:19 +0430

    FCRMenuTitleCV created

---
+++ Luncheon/Views/FCRMenuTitleCV.swift
@@ -0,0 +1,28 @@
+//
+//  FCRMenuTitleCV.swift
+//  Luncheon
+//
+//  Created by Arshiya on 1/23/1399 AP.
+//  Copyright © 1399 AP ir.luncheon. All rights reserved.
+//
+
+import UIKit
+
+class FCRMenuTitleCV: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
