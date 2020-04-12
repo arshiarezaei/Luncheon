256389b8fed62ff26dfc568f9bd241988588cf0e
Arshiya Rezaie <arshiyarezaie@gmail.com>
2020-04-12 15:44:50 +0430

    FoodCVCell created

---
+++ Luncheon/Views/FoodCVCell.swift
@@ -0,0 +1,20 @@
+//
+//  FoodCVCell.swift
+//  Luncheon
+//
+//  Created by Arshiya on 1/24/1399 AP.
+//  Copyright © 1399 AP ir.luncheon. All rights reserved.
+//
+
+import UIKit
+
+class FoodCVCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
