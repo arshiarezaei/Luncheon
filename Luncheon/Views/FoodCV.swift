d9a5418d0dc14d2bdbd56f866cb080cdfa21e4be
Arshiya Rezaie <arshiyarezaie@gmail.com>
2020-04-12 15:29:32 +0430

    FoodCV created

---
+++ Luncheon/Views/FoodCV.swift
@@ -0,0 +1,22 @@
+//
+//  FoodCV.swift
+//  Luncheon
+//
+//  Created by Arshiya on 1/24/1399 AP.
+//  Copyright © 1399 AP ir.luncheon. All rights reserved.
+//
+
+import UIKit
+
+class FoodCV: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        debugPrint("class FoodCV->init")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
