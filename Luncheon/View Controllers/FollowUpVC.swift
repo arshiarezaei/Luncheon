96f7ee2a9d008688215b87709f533d6659317816
arshiya rezaie <arshia.rezaee@yahoo.com>
2020-09-19 20:12:38 +0430

    orderStatus added to followUpVC

--- Luncheon/View Controllers/FollowUpVC.swift
+++ Luncheon/View Controllers/FollowUpVC.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class FollowUpVC: UIViewController {
     private let navBar : UINavigationBar = UINavigationBar(frame:.zero)
+    private let orderStatus:OrderStatusView = OrderStatusView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .whiteBackgroud
         view.addSubview(navBar)
         setupNavBar()
+        
+        self.view.addSubview(orderStatus)
+        setupOrderStatusVC()
     }
     private func setupNavBar() {
         navBar.translatesAutoresizingMaskIntoConstraints = false
@@ -29,5 +33,15 @@ class FollowUpVC: UIViewController {
             navBar.safeAreaLayoutGuide.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor),
         ])
     }
-
+    private func setupOrderStatusVC() {
+        orderStatus.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            orderStatus.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            orderStatus.topAnchor.constraint(equalTo: navBar.bottomAnchor),
+            orderStatus.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+            orderStatus.heightAnchor.constraint(equalToConstant: 90),
+        ])
+    }
+    
 }
