0017454454faede8341fbb10029ffa5f32d64574
arshiya rezaie <arshia.rezaee@yahoo.com>
2020-09-24 17:34:33 +0330

    lightGray added to UIColor extension.

--- Luncheon/Extentions/Extention UIColor.swift
+++ Luncheon/Extentions/Extention UIColor.swift
@@ -13,5 +13,5 @@ extension UIColor{
     static let luncehonLogoText:UIColor = UIColor(red: 170/255, green: 30/255, blue: 30/255, alpha: 1)
     static let whiteBackgroud:UIColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
     static let whiteBorder:UIColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1)
-    
+    static let lightGray:UIColor = UIColor(red: 112/255, green: 112/255, blue: 112/255, alpha: 1)
 }
