c35640ebcff3e7cedf82c5a612643ba098c6f884
arshiya rezaie <arshia.rezaee@yahoo.com>
2020-09-30 00:44:37 +0330

    LoginAlertView implemented

---
+++ Luncheon/Views/LoginAlertView.swift
@@ -0,0 +1,39 @@
+//
+//  LoginAlertView.swift
+//  Luncheon
+//
+//  Created by arshiya on 9/30/20.
+//  Copyright © 2020 ir.luncheon. All rights reserved.
+//
+
+import UIKit
+
+class LoginAlertView: UIView {
+    private var text:UILabel = {
+        var l = UILabel(frame: .zero)
+        l.text = "برای مشاهده این صفحه ابتدا وارد حساب کاربری خود شوید"
+        l.translatesAutoresizingMaskIntoConstraints = false
+        return l
+    }()
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        // add label
+        self.addSubview(text)
+        setupText()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func setupText() {
+        NSLayoutConstraint.activate([
+            text.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            text.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            text.widthAnchor.constraint(equalTo: self.widthAnchor),
+            text.heightAnchor.constraint(equalTo: self.heightAnchor),
+        ])
+    }
+
+}
