6412db374bc9647beb72663e563c3b11bf591234
arshiya rezaie <arshia.rezaee@yahoo.com>
2020-10-19 15:25:24 +0330

    sample data added

--- Luncheon/SampleData/SampleDataManager.swift
+++ Luncheon/SampleData/SampleDataManager.swift
@@ -40,21 +40,22 @@ struct SampleRestaurantManager {
     ]
 
     private static let SubwayFood:[SampleFood] = [
-        SampleFood(id: 0, persianName: "هات داگ", rate: 5.0, price: 40000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "food")!),
-        SampleFood(id: 1, persianName: "ژامبون تنوری", rate: 4.6, price: 20000, discountAmount: 1000, ingredients: [], calorie: 0,image: UIImage(named: "food")!),
+        SampleFood(id: 0, persianName: "هات داگ", rate: 5.0, price: 40000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "hot dog")!),
+        SampleFood(id: 1, persianName: "ژامبون تنوری", rate: 4.6, price: 20000, discountAmount: 1000, ingredients: [], calorie: 0,image: UIImage(named: "sand")!),
+        SampleFood(id: 2, persianName: "چیلی داگ", rate: 4.6, price: 20000, discountAmount: 1000, ingredients: [], calorie: 0,image: UIImage(named: "chili")!)
     ]
 
     private static let mcDonald:[SampleFood] = [
-        SampleFood(id: 0, persianName: "مرغ سخاری", rate: 5.0, price: 40000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "food")!),
-        SampleFood(id: 1, persianName: "کتف", rate: 4.6, price: 20000, discountAmount: 1000, ingredients: [], calorie: 0,image: UIImage(named: "1-1-1")!),
+        SampleFood(id: 0, persianName: "مرغ سخاری", rate: 5.0, price: 40000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "8")!),
+        SampleFood(id: 1, persianName: "کتف", rate: 4.6, price: 20000, discountAmount: 1000, ingredients: [], calorie: 0,image: UIImage(named: "10")!),
     ]
     private static let mcDonalFoodBurger:[SampleFood] = [
-        SampleFood(id: 2, persianName: "دونالد برگر", rate: 5.0, price: 40000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "food")!),
-        SampleFood(id: 3, persianName: "دونالد برگر ويژه", rate: 4.6, price: 45000, discountAmount: 1000, ingredients: [], calorie: 0,image: UIImage(named: "food")!),
-        SampleFood(id: 4, persianName: "چیز برگر", rate: 3.6, price: 50000, discountAmount: 2000, ingredients: [], calorie: 0,image: UIImage(named: "food")!),
-        SampleFood(id: 5, persianName: "چیزبرگر ویژه", rate: 5.0, price: 55000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "food")!),
-        SampleFood(id: 6, persianName: "چیکن برگر", rate: 5.0, price: 35000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "food")!),
-        SampleFood(id: 7, persianName: "ماشروم برگر", rate: 5.0, price: 40000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "food")!),
+        SampleFood(id: 2, persianName: "دونالد برگر", rate: 5.0, price: 40000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "burger1")!),
+        SampleFood(id: 3, persianName: "دونالد برگر ويژه", rate: 4.6, price: 45000, discountAmount: 1000, ingredients: [], calorie: 0,image: UIImage(named: "2")!),
+        SampleFood(id: 4, persianName: "چیز برگر", rate: 3.6, price: 50000, discountAmount: 2000, ingredients: [], calorie: 0,image: UIImage(named: "3")!),
+        SampleFood(id: 5, persianName: "چیزبرگر ویژه", rate: 5.0, price: 55000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "4")!),
+        SampleFood(id: 6, persianName: "چیکن برگر", rate: 5.0, price: 35000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "5")!),
+        SampleFood(id: 7, persianName: "ماشروم برگر", rate: 5.0, price: 40000, discountAmount: 0, ingredients: [], calorie: 0,image: UIImage(named: "6")!),
     ]
    
     private static let mcDonaldFood = mcDonald + mcDonalFoodBurger
